8f13277907c4a3afe5c22e239bf39811fe205daf
Mohamed <muhamedrabi3@icloud.com>
2019-12-10 22:25:35 +0200

    stable version of dice

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var di1: UIImageView!
+    
+    @IBOutlet weak var di2: UIImageView!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+
+  
+    @IBAction func brn(_ sender: UIButton) {
+        var c = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+        
+        c.shuffle()
+        di1.image=c[0]
+        di2.image=c.randomElement()
+       
+    }
 
 }
 
